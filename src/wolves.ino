f52500c2dff95fe3c5d038e52a737da9f49cb18a
Jacques Supcik <jacques.supcik@hefr.ch>
2016-05-07 17:33:34 +0200

    Add calls to begin

--- src/wolves.ino
+++ src/wolves.ino
@@ -4,26 +4,32 @@
 #include "RFIDSensor.h"
 #include "Solenoid.h"
 
-LED leds[] = {
+#define N_OF_QUESTIONS 3
+
+LED leds[N_OF_QUESTIONS] = {
     LED(0,0),
     LED(0,0),
     LED(0,0)
 };
 
-RFIDSensor sensors[] = {
+RFIDSensor sensors[N_OF_QUESTIONS] = {
     RFIDSensor(0, 0),
     RFIDSensor(0, 0),
     RFIDSensor(0, 0)
 };
 
-Solenoid solenoids[] = {
+Solenoid solenoids[N_OF_QUESTIONS] = {
     Solenoid(0, sensors[0], leds[0]),
     Solenoid(0, sensors[1], leds[1]),
     Solenoid(0, sensors[2], leds[2]),
 };
 
 void setup() {
-
+    for (int i = 0; i < N_OF_QUESTIONS; i++) {
+        leds[i].begin();
+        sensors[i].begin();
+        solenoids[i].begin();
+    }
 }
 
 
