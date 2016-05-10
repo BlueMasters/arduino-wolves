d7f1de5d87a870b4d7d038b43b510eaa0b6e13f9
Jacques Supcik <jacques.supcik@hefr.ch>
2016-05-10 17:08:26 +0200

    Add testing routines

--- src/wolves.ino
+++ src/wolves.ino
@@ -9,31 +9,64 @@
 #define N_OF_QUESTIONS 3
 
 LED leds[N_OF_QUESTIONS] = {
-    LED(0,0),
-    LED(0,0),
-    LED(0,0)
+    LED(23,24),
+    LED(33,34),
+    LED(43,44)
 };
 
 RFIDSensor sensors[N_OF_QUESTIONS] = {
-    RFIDSensor(0, 0, 0),
-    RFIDSensor(1, 0, 0),
-    RFIDSensor(2, 0, 0)
+    RFIDSensor(0, 22, 53),
+    RFIDSensor(1, 32, 53),
+    RFIDSensor(2, 42, 53)
 };
 
 Solenoid solenoids[N_OF_QUESTIONS] = {
-    Solenoid(0, sensors[0], leds[0]),
-    Solenoid(0, sensors[1], leds[1]),
-    Solenoid(0, sensors[2], leds[2]),
+    Solenoid(25, sensors[0], leds[0]),
+    Solenoid(35, sensors[1], leds[1]),
+    Solenoid(45, sensors[2], leds[2]),
 };
 
-void setup() {
-    app.statusLed = new RGBLED(0,0,0);
+void testStatusLed() {
+    app.statusLed.setColor(COLOR_RED);
+    delay(1000);
+    app.statusLed.setColor(COLOR_GREEN);
+    delay(1000);
+    app.statusLed.setColor(COLOR_BLUE);
+    delay(1000);
+    app.statusLed.off();
+}
+
+void testQuestions() {
     for (int i = 0; i < N_OF_QUESTIONS; i++) {
-        leds[i].begin();
-        sensors[i].begin();
-        solenoids[i].begin();
+        leds[i].red();
+        solenoids[i].on();
+        delay(200);
+    }
+    delay(1000);
+    for (int i = 0; i < N_OF_QUESTIONS; i++) {
+        leds[i].green();
+        solenoids[i].off();
+        delay(200);
+    }
+    delay(1000);
+    for (int i = 0; i < N_OF_QUESTIONS; i++) {
+        leds[i].off();
     }
 }
+
+void setup() {
+    Serial.begin(9600);
+    app.statusLed.begin(2,3,4);
+    for (int i = 0; i < N_OF_QUESTIONS; i++) {
+        leds[i].begin();
+        //sensors[i].begin();
+        solenoids[i].begin();
+    }
+    // Testing
+    testStatusLed();
+    testQuestions();
+}
+
 
 
 void loop() {}
