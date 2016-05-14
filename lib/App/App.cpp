b868a317c2306456e2388eef83108ed5ea34b989
Jacques Supcik <jacques.supcik@hefr.ch>
2016-05-14 17:12:16 +0200

    Add draft of readconfig

--- lib/App/App.cpp
+++ lib/App/App.cpp
@@ -1,3 +1,18 @@
 #include "App.h"
+#include <Streaming.h>
+#include <EEPROM.h>
+
+#define SERIAL_INIT_TIMEOUT 1500
+#define SERIAL_TIMEOUT 1500
 
 App app;
+
+void App::readConfig() {
+    Serial << "Press ENTER now if you want to reset the PIN code." << endl;
+    Serial.setTimeout(SERIAL_INIT_TIMEOUT);
+    if (Serial.read() == '\r') {
+        Serial << "Enter the new PIN and press ENTER." << endl;
+        
+    }
+    Serial.setTimeout(SERIAL_TIMEOUT);
+}
--- lib/App/App.h
+++ lib/App/App.h
@@ -8,6 +8,7 @@
 #define DEBUG true
 #define DI_GRANULARITY 100 // 1 unit means +100 ms
 #define DF_GRANULARITY 25  // 1 unit means 25 ms
+#define MAGIC "B1M"
 
 enum globalMode {globmode_NORMAL, globmode_LEARN };
 enum configMode {confmode_DI, confmode_DF, confmode_None};
@@ -22,6 +23,7 @@ public:
     uint8_t DI; // TODO use a more descriptive name or comment
     uint8_t DF; // TODO use a more descriptive name or comment
     RGBLED statusLed;
+    void readConfig();
 };
 
 extern App app;
