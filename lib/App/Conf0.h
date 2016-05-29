6810752a4217eee567aa1af9a2b65914404cfb1f
Lucy Linder <lucy.derlin@gmail.com>
2016-05-29 10:38:17 +0200

    add space after headers

--- lib/App/Conf0.h
+++ lib/App/Conf0.h
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *****************************************************************************/
+ 
 #ifndef CONF0__H
 #define CONF0__H
 
